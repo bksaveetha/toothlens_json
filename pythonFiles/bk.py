9a8e45d4ac4495eb30b43c34fadc4aaa61ae32bb
bksaveetha <mishrabk1985088@gmail.com>
2021-06-28 22:06:46 +0530

    function updated

--- pythonFiles/bk.py
+++ pythonFiles/bk.py
@@ -1,21 +1,78 @@
 import json
 
-a_file = open("mish.json", "r") 
-json_object = json.load(a_file)
+
+json_object = {
+    "Age": 35,
+    "medicine_that_reduce_salaiva": "Yes",
+    "chemo_radiation_therapy": "Yes",
+    "Diabetes": "Yes",
+    "pregnancy_and_hormonal_changes": "Yes",
+    "Acid reflux": "Yes",
+    "Fluoride exposure": "Yes",
+    "Special_healthcare_need": "No",
+    "Drug/Alcohol": "No",
+    "Family history of cavities and gum infections": "Yes",
+    "osteroporosis in women": "Yes",
+
+    "Daily drinking water consumption": "at mealtimes only",
+
+
+    "Sugary food and drinks": "primarily at mealtimes",
+    "Smoking and tobacco use": "No",
+    "Toothbrushing": "twice daily",
+    "Flossing": "twice daily",
+    "Mouthwash": "twice daily",
+    "Replace toothbrush": "once in 3 months",
+
+
+
+
+
+
+
+
+
+    "Cavities or treatment for cavities in last 36 months": "more then 3 cavities",
+    "Dental Scaling/Gum treatment done ": "within one year",
+    "Braces/Aligner treatments currently": "aligner",
+    "Missing teeth due to cavities or gum problems in last 36 months": "no",
+    "Crowns/fixed and removable tooth replacements done before 36 months": "no",
+    "unhealed mouth ulcer or swelling  currently": "10-14 days",
+    "gum bleeding present currently": "occasionally",
+    "pain/sensitivity in any tooth currently": "occasionally",
+    "food getting stuck in teeth currently": "occasionally",
+
+
+
+
+
+
+
+
+    "Endocarditis and Cardiovascular disease": "good dental health",
+    "Premature birth and low weight in children": "good dental health",
+    "Covid complications": "good dental health",
+    "Pneumonia": "good dental health",
+    "skin problems": "good dental health"
+}
+
 
 
 def GeneralHealth(json_object):
+    
     new_object = {}
-    
-    
 
     
     if "Age" in json_object.keys():
-        if json_object["Age"]< 30:
-            new_object ["Age"] = "Low"
+        if json_object["Age"] < 30:
+            new_object["Age"] = "Low"
+            
         else:
             new_object["Age"] = "Medium"
-    
+        
+        
+        
+        
 
     if "Diabetes" in json_object.keys():
         if json_object["Diabetes"] == "Yes":
@@ -29,12 +86,14 @@ def GeneralHealth(json_object):
         if json_object["chemo_radiation_therapy"] == "Yes":
             new_object["chemo_radiation_therapy"] = "High"
         else:
-            new_object["chemo_radiation_therapy"] = "Low"
-
+            new_object["chemo_radiation_therapy"] = "Low or Medium"
 
     
-
    
+
+
+
+
 
 
     if "Fluoride exposure" in json_object.keys():
@@ -119,15 +178,15 @@ def GeneralHealth(json_object):
             new_object["Daily drinking water consumption"] = "Low"
 
 
+    
+    
 
 
 
 
+    
 
-
-
-
-
+   
 
     if "Sugary food and drinks" in json_object.keys():
         if json_object["Sugary food and drinks"] == "primarily at mealtimes":
@@ -164,7 +223,7 @@ def GeneralHealth(json_object):
             new_object["Mouthwash"] = "Low"
         else:
             new_object["Mouthwash"] = "High"
-
+    
     if "Replace toothbrush" in json_object.keys():
         if json_object["Replace toothbrush"] == "once in 3 months":
             new_object["Replace toothbrush"] = "Low"
@@ -172,127 +231,125 @@ def GeneralHealth(json_object):
             new_object["Replace toothbrush"] = "Medium"
 
 
-        
-        
+    
+    
 
-        
+    
 
-        if "Cavities or treatment for cavities in last 36 months" in json_object.keys():
-            if json_object["Cavities or treatment for cavities in last 36 months"] == "more then 3 cavities":
-                new_object["Cavities or treatment for cavities in last 36 months"] = "High"
-            else:
-                new_object["Cavities or treatment for cavities in last 36 months"] = "Medium"
-
-
-
-        if "Dental Scaling/Gum treatment done " in json_object.keys():
-            if json_object[ "Dental Scaling/Gum treatment done "] == "within one year":
-                new_object[ "Dental Scaling/Gum treatment done "] = "Low"
-            else:
-                new_object[ "Dental Scaling/Gum treatment done "] = "Medium"
-
-
-        if "Braces/Aligner treatments currently" in json_object.keys():
-            if json_object["Braces/Aligner treatments currently"] == "aligner":
-                new_object["Braces/Aligner treatments currently"] = "Medium"
-            else:
-                new_object["Braces/Aligner treatments currently"] = "Low"
-
-
-        if "Missing teeth due to cavities or gum problems in last 36 months" in json_object.keys():
-            if json_object["Missing teeth due to cavities or gum problems in last 36 months"] == "no":
-                new_object["Missing teeth due to cavities or gum problems in last 36 months"] = "Low"
-            else:
-                new_object["Missing teeth due to cavities or gum problems in last 36 months"] = "high"
-
-        if "Crowns/fixed and removable tooth replacements done before 36 months" in json_object.keys():
-            if json_object["Crowns/fixed and removable tooth replacements done before 36 months"] == "no":
-                new_object["Crowns/fixed and removable tooth replacements done before 36 months"] = "Low"
-            else:
-                new_object["Crowns/fixed and removable tooth replacements done before 36 months"] = "high"
-
-
-        if "unhealed mouth ulcer or swelling  currently" in json_object.keys():
-            if json_object["unhealed mouth ulcer or swelling  currently"] ==  "10-14 days":
-                new_object["unhealed mouth ulcer or swelling  currently"] = "Low"
-            else:
-                new_object["unhealed mouth ulcer or swelling  currently"] = "High"
+    if "Cavities or treatment for cavities in last 36 months" in json_object.keys():
+        if json_object["Cavities or treatment for cavities in last 36 months"] == "more then 3 cavities":
+            new_object["Cavities or treatment for cavities in last 36 months"] = "High"
+        else:
+            new_object["Cavities or treatment for cavities in last 36 months"] = "Medium"
 
 
 
-
-        if "gum bleeding present currently" in json_object.keys():
-            if json_object["gum bleeding present currently"] ==  "occasionally":
-                new_object["gum bleeding present currently"] = "Low"
-            else:
-                new_object["gum bleeding present currently"] = "High"
-
+    if "Dental Scaling/Gum treatment done " in json_object.keys():
+        if json_object[ "Dental Scaling/Gum treatment done "] == "within one year":
+            new_object[ "Dental Scaling/Gum treatment done "] = "Low"
+        else:
+            new_object[ "Dental Scaling/Gum treatment done "] = "Medium"
 
 
-        if "pain/sensitivity in any tooth currently" in json_object.keys():
-            if json_object["pain/sensitivity in any tooth currently"] ==  "occasionally":
-                new_object["pain/sensitivity in any tooth currently"] = "Low"
-            else:
-                new_object["pain/sensitivity in any tooth currently"] = "High"
+    if "Braces/Aligner treatments currently" in json_object.keys():
+        if json_object["Braces/Aligner treatments currently"] == "aligner":
+            new_object["Braces/Aligner treatments currently"] = "Medium"
+        else:
+            new_object["Braces/Aligner treatments currently"] = "Low"
 
 
+    if "Missing teeth due to cavities or gum problems in last 36 months" in json_object.keys():
+        if json_object["Missing teeth due to cavities or gum problems in last 36 months"] == "no":
+           new_object["Missing teeth due to cavities or gum problems in last 36 months"] = "Low"
+        else:
+            new_object["Missing teeth due to cavities or gum problems in last 36 months"] = "high"
 
-        if "food getting stuck in teeth currently" in json_object.keys():
-            if json_object["food getting stuck in teeth currently"] ==  "occasionally":
-                new_object["food getting stuck in teeth currently"] = "Low"
-            else:
-                new_object["food getting stuck in teeth currently"] = "High"
-
-        
-        
+    if "Crowns/fixed and removable tooth replacements done before 36 months" in json_object.keys():
+        if json_object["Crowns/fixed and removable tooth replacements done before 36 months"] == "no":
+            new_object["Crowns/fixed and removable tooth replacements done before 36 months"] = "Low"
+        else:
+            new_object["Crowns/fixed and removable tooth replacements done before 36 months"] = "high"
 
 
-        
-
-
-        if "Endocarditis and Cardiovascular disease" in json_object.keys():
-            if json_object["Endocarditis and Cardiovascular disease"] ==  "good dental health":
-                new_object["Endocarditis and Cardiovascular disease"] = "Low"
-            else:
-                new_object["Endocarditis and Cardiovascular disease"] = "Medium"
+    if "unhealed mouth ulcer or swelling  currently" in json_object.keys():
+        if json_object["unhealed mouth ulcer or swelling  currently"] ==  "10-14 days":
+           new_object["unhealed mouth ulcer or swelling  currently"] = "Low"
+        else:
+            new_object["unhealed mouth ulcer or swelling  currently"] = "High"
 
 
 
-        if "Premature birth and low weight in children" in json_object.keys():
-            if json_object["Premature birth and low weight in children"] == "good dental health":
-                new_object["Premature birth and low weight in children"] = "Low"
-            else:
-                new_object["Premature birth and low weight in children"] = "Medium"
+
+    if "gum bleeding present currently" in json_object.keys():
+        if json_object["gum bleeding present currently"] ==  "occasionally":
+            new_object["gum bleeding present currently"] = "Low"
+        else:
+            new_object["gum bleeding present currently"] = "High"
 
 
 
-        if "Covid complications" in json_object.keys():
-            if json_object["Covid complications"] == "good dental health":
-                new_object["Covid complications"] = "Low"
-            else:
-                new_object["Covid complications"] = "Medium"
+    if "pain/sensitivity in any tooth currently" in json_object.keys():
+        if json_object["pain/sensitivity in any tooth currently"] ==  "occasionally":
+            new_object["pain/sensitivity in any tooth currently"] = "Low"
+        else:
+            new_object["pain/sensitivity in any tooth currently"] = "High"
 
 
 
-        if "Pneumonia" in json_object.keys():
-            if json_object["Pneumonia"] == "good dental health":
-                new_object["Pneumonia"] = "Low"
-            else:
-                new_object["Pneumonia"] = "Medium"
+    if "food getting stuck in teeth currently" in json_object.keys():
+        if json_object["food getting stuck in teeth currently"] ==  "occasionally":
+            new_object["food getting stuck in teeth currently"] = "Low"
+        else:
+            new_object["food getting stuck in teeth currently"] = "High"
+
+    
+    
+
+
+    
+
+
+    if "Endocarditis and Cardiovascular disease" in json_object.keys():
+        if json_object["Endocarditis and Cardiovascular disease"] ==  "good dental health":
+           new_object["Endocarditis and Cardiovascular disease"] = "Low"
+        else:
+            new_object["Endocarditis and Cardiovascular disease"] = "Medium"
 
 
 
-        if "skin problems" in json_object.keys():
-            if json_object["skin problems"] == "good dental health":
-                new_object["skin problems"] = "Low"
-            else:
-                new_object["skin problems"] = "Medium"
+    if "Premature birth and low weight in children" in json_object.keys():
+        if json_object["Premature birth and low weight in children"] == "good dental health":
+            new_object["Premature birth and low weight in children"] = "Low"
+        else:
+            new_object["Premature birth and low weight in children"] = "Medium"
 
+
+
+    if "Covid complications" in json_object.keys():
+        if json_object["Covid complications"] == "good dental health":
+            new_object["Covid complications"] = "Low"
+        else:
+            new_object["Covid complications"] = "Medium"
+
+
+
+    if "Pneumonia" in json_object.keys():
+        if json_object["Pneumonia"] == "good dental health":
+            new_object["Pneumonia"] = "Low"
+        else:
+            new_object["Pneumonia"] = "Medium"
+
+
+
+    if "skin problems" in json_object.keys():
+        if json_object["skin problems"] == "good dental health":
+            new_object["skin problems"] = "Low"
+        else:
+            new_object["skin problems"] = "Medium"
 
 
     print(new_object)
-
-print(GeneralHealth(json_object))   
+print(GeneralHealth(json_object))
     
 
 
